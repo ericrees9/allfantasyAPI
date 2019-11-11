f602e7a5a88f6e8c2974d60417e3d803c5973318
erees <erees@protonmail.com>
2019-11-11 16:51:47 -0500

    Setting up initial folder structure.

---
+++ index.js
@@ -0,0 +1,20 @@
+// .ENV Linking
+require("dotenv").config();
+
+// Express Linking
+let express = require("express");
+let app = express();
+let cors = require("cors");
+
+app.use(express.json());
+app.use(cors());
+
+
+
+// Connecting to PGAdmin
+sequelize.sync(); // {force: true} for resetting tables
+
+// Listening Message
+app.listen(process.env.PORT, function(){
+    console.log(`App is listening on port ${process.env.PORT}!`)
+});
